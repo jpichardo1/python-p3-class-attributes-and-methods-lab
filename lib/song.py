517946f8dd6f1451f7d9a4db9633a5fdc194a49e
Jessica <jess.pichardo18@gmail.com>
2024-06-06 00:12:17 -0700

    complete

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,55 @@
 class Song:
+
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.artist = artist
+        self.genre = genre
+        self.name = name
+
+        Song.count += 1
+        Song.add_song_to_count(self)
+
+    @classmethod
+    def add_song_to_count(cls, song):
+        cls.add_to_genres(song.genre)
+        cls.add_to_artists(song.artist)
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+    
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
     pass
